3a24d6423a3e7ef30fed13e785760f8ea32a9c5f
James Hill <jahill@groupon.com>
2021-05-04 11:27:23 +0100

    Added dual bundle config for module export

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,20 +4,27 @@ import * as path from 'path';
 
 /* -----------------------------------
  *
- * Config
+ * Output
  *
  * -------------------------------- */
 
-const config = ({ mode }): Configuration => ({
+const outputFiles = [
+  { target: 'es5', filename: '[name].js' },
+  { target: 'es2017', filename: '[name].module.js' },
+];
+
+/* -----------------------------------
+ *
+ * Default
+ *
+ * -------------------------------- */
+
+const defaultConfig = {
   entry: {
     index: path.join(__dirname, './src/define.ts'),
   },
-  mode: mode || 'development',
-  target: 'es5',
   externals: [nodeExternals()],
-  devtool: mode === 'development' ? 'eval-source-map' : void 0,
-  context: path.join(__dirname, `./src`),
-  cache: mode === 'development',
+  context: path.join(__dirname, './src'),
   output: {
     path: path.join(__dirname, './dist'),
     filename: '[name].js',
@@ -34,36 +41,54 @@ const config = ({ mode }): Configuration => ({
     __filename: true,
     __dirname: true,
   },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              compilerOptions: {
-                target: 'es5',
-              },
-            },
-          },
-        ],
-      },
-    ],
-  },
-  performance: {
-    hints: mode === 'development' ? 'warning' : void 0,
-  },
-  plugins: [
-    new DefinePlugin({
-      __DEV__: mode === 'development',
-    }),
-  ],
   stats: {
     colors: true,
     timings: true,
   },
-});
+};
+
+/* -----------------------------------
+ *
+ * Config
+ *
+ * -------------------------------- */
+
+const config = ({ mode }): Configuration[] =>
+  outputFiles.map(({ target, filename }) => ({
+    ...defaultConfig,
+    target,
+    devtool: mode === 'development' ? 'eval-source-map' : void 0,
+    cache: mode === 'development',
+    output: {
+      ...defaultConfig.output,
+      filename,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                compilerOptions: {
+                  target,
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+    performance: {
+      hints: mode === 'development' ? 'warning' : void 0,
+    },
+    plugins: [
+      new DefinePlugin({
+        __DEV__: mode === 'development',
+      }),
+    ],
+  }));
 
 /* -----------------------------------
  *
